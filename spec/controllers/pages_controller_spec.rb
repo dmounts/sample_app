32229008276d4e9acf0fd54b116270e893984490
dmounts <duston_mounts@hotmail.com>
2011-04-04 21:26:29 -0500

    updated routes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -44,7 +44,7 @@ describe PagesController do
 
   end
 
-   describe "GET 'about'" do
+  describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
@@ -61,5 +61,24 @@ describe PagesController do
     end
 
   end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the correct title" do
+      get 'help'
+      response.should have_selector("title", :content => "#{@base_title} | Help")
+    end
+
+    it "should have a non-blank body" do
+      get 'help'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
+
+  end
+
 
 end
